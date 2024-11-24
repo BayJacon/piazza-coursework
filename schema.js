523d1b32a5e84beab77d7612d41f39ca38926362
James Bacon <jaybacon234@gmail.com>
2024-11-24 21:32:48 +0000

    init schema implementation

---
+++ schema.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+    user: { 
+        type: String, 
+        required: true },
+    title: { type: String, 
+        required: true },
+    text: { type: String, 
+        required: true },
+    topic: { type: String, 
+        required: true },
+    expirationTime: { type: Date, 
+        required: true },
+    status: { type: String, 
+        default: "Live" },
+    likes: { type: Number, 
+        default: 0 },  
+    dislikes: { type: Number, 
+        default: 0 },  
+    comments: [{ user: String, text: String,
+        date: Date }],
+    date: { type: Date, 
+        default: Date.now }
+});
