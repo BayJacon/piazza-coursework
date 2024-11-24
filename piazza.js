65c0bd338f8a49e3648cafe91fd0818ff8a88b45
James Bacon <jaybacon234@gmail.com>
2024-11-24 22:05:23 +0000

    commenting feature to piazza.js

--- piazza.js
+++ piazza.js
@@ -9,3 +9,22 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
+
+router.post('/:id/comment', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).send('Post not found');
+
+        const comment = {
+            user: req.body.user,
+            text: req.body.text,
+            date: new Date()
+        };
+
+        post.comments.push(comment);
+        await post.save();
+        res.status(201).json({ message: 'Comment added!', comments: post.comments });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
