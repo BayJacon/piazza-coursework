bd8a424b685ba66cdca6d342c0c7b579cc993475
James Bacon <jaybacon234@gmail.com>
2024-12-07 15:06:33 +0000

    template schema for User.js added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const User = require('../models/User');
+const User = require('../models/User.js');
 const {registerValidation,loginValidation} = require('../validations/validation')
 
 const bcryptjs = require('bcryptjs')
